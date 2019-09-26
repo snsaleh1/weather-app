74185282c11c7ce245f4a133b93d288211bb3e3f
snsaleh1 <snsaleh11@gmail.com>
2019-09-26 17:53:58 -0500

    api test complete

--- src/App.js
+++ src/App.js
@@ -4,11 +4,20 @@ import Titles from './components/titles';
 import Weather from './components/weather';
 
 class App extends React.Component {
+  getWeather = async (e) => {
+    e.preventDefault();
+    //e-variable passing to function / target-where it's coming from/ element-just remember it/ city.value - value of input field
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`);
+    const response = await api_call.json();
+    console.log(response);
+  }
   render() {
     return(
       <div>
         <Titles />
-        <Form />
+        <Form loadWeather={this.getWeather} />
         <Weather />
       </div>
     )
