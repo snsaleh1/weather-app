4b74dba18ecbf94f2f9ef8db6347af550266bdac
snsaleh1 <snsaleh11@gmail.com>
2019-09-26 17:23:25 -0500

    base display set key added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Form from './components/form';
+import Titles from './components/titles';
+import Weather from './components/weather';
 
 class App extends React.Component {
   render() {
     return(
       <div>
-
+        <Titles />
+        <Form />
+        <Weather />
       </div>
     )
   }
---
+++ src/components/weather.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Weather = (props) => {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default Weather;
